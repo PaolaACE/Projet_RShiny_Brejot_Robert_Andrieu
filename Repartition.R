4cb29f01d828a3ccf3dc48acb45386662fd3defc
Augustin <augustinrobert03@gmail.com>
2024-09-25 15:06:48 +0200

    Import validé du jeu de données

--- Repartition.R
+++ Repartition.R
@@ -1,11 +1,21 @@
 library(data.table)
 
 data <- fread(file = "fr-esr-insertion_professionnelle-master.csv", 
-                   header = TRUE, sep = ";", stringsAsFactors = TRUE)
+                   header = TRUE, sep = ";", stringsAsFactors = F)
 
-summary(data)
+data[data == 'ns'] = NA
+data[data == 'nd'] = NA
 
+data[, (14:32) := lapply(.SD, as.numeric), .SDcols = 14:32]
+data[, (2:31) := lapply(.SD, as.factor), .SDcols = (2:31)]
+data[, (33:35) := lapply(.SD, as.factor), .SDcols = (33:35)]
 
+summary(data$taux_d_emploi)
+class(data$etablissement)
+class(data$id_paysage)
+
+summary(data$taux_d_emploi)
+dim(data)
 
 #annee
 #diplome
@@ -44,14 +54,18 @@ summary(data)
 #id_paysage
 
 summary(data$taux_d_emploi)
+summary(data$diplome)
+summary(data$emplois_cadre_ou_professions_intermediaires)
+summary(data$situation)
 
-table(data$etablissement, data$cle_etab)
+table(data$etablissementactuel, data$etablissement)
+
 
 #axes d'analyses
 
 #Proportion et repartition des emplois par domaine
 #Proportion et repartition des salaires par domaine 
-#
+#Repartition des emplois en fonction de la situation (18 ou 30 mois après le diplôme)
 
 
 #penser a mettre a la fin
