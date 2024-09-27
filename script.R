ee88729529d0293f32ead5da19456a1e5555d279
Ameliee12 <amelie.brejot@wanadoo.fr>
2024-09-27 16:29:54 +0200

    carte 2.0

--- script.R
+++ script.R
@@ -2,15 +2,17 @@
 library(data.table)
 library(dplyr)
 library(tidygeocoder)
+library(sf)
 
 data <- fread(file = "fr-esr-insertion_professionnelle-master.csv", 
-              header = TRUE, sep = ";", stringsAsFactors = TRUE)
+              header = TRUE, sep = ";", stringsAsFactors = TRUE, na.strings = "")
 data$taux_de_reponse <- as.numeric(data$taux_de_reponse)
+data$taux_dinsertion <- as.numeric(data$taux_dinsertion)
 
 summary(data)
 
 df <- data %>% group_by(academie) %>%
-  summarise(taux_reponse_totale = mean(taux_de_reponse))
+  summarise(taux_reponse_totale = mean(taux_de_reponse), taux_insertion_moyen = mean(taux_dinsertion))
 
 df <- df[2:30,]
 df$academie <- as.character(df$academie)
@@ -18,12 +20,13 @@ df$academie <- as.character(df$academie)
 df <- df %>%
   geocode(academie, method = 'osm', full_results = TRUE)
 
-df <- df[,1:4]
+df <- df[,1:5]
 
 library(leaflet)
 
 leaflet(df) %>%
   addTiles() %>%
+  setView(lng = 2.2137, lat = 46.2276, zoom = 6) %>% 
   addCircleMarkers(
     lng = ~long,
     lat = ~lat,
@@ -31,5 +34,11 @@ leaflet(df) %>%
     color = "blue",
     fillOpacity = 0.8,
     popup = ~paste("<strong>Académie :</strong>", academie, "<br>",
-                   "<strong>Taux de réponse moyen :</strong>", round(taux_reponse_totale), "%")
+                   "<strong>Taux de réponse moyen :</strong>", round(taux_reponse_totale), "%", "<br>",
+                   "<strong>Taux d'insertion moyen :</strong>", round(taux_insertion_moyen), "%")
   )
+
+data %>% group_by(annee) %>% summarise(salaire_brut_annuel_estime = mean(salaire_brut_annuel_estime))
+plot(data$annee, data$salaire_brut_annuel_estime)
+
+
--- server.R
+++ server.R
@@ -9,6 +9,7 @@
 library(data.table)
 library(shiny)
 library(tidygeocoder)
+library(dplyr)
 library(leaflet)
 
 data <- fread(file = "fr-esr-insertion_professionnelle-master.csv", 
@@ -18,7 +19,7 @@ data$taux_de_reponse <- as.numeric(data$taux_de_reponse)
 summary(data)
 
 df <- data %>% group_by(academie) %>%
-  summarise(taux_reponse_totale = mean(taux_de_reponse))
+  summarise(taux_reponse_totale = mean(taux_de_reponse), taux_insertion_moyen = mean(taux_dinsertion))
 
 df <- df[2:30,]
 df$academie <- as.character(df$academie)
@@ -26,7 +27,7 @@ df$academie <- as.character(df$academie)
 df <- df %>%
   geocode(academie, method = 'osm', full_results = TRUE)
 
-df <- df[,1:4]
+df <- df[,1:5]
 
 
 # Define server logic required to draw a histogram
@@ -36,6 +37,7 @@ function(input, output, session) {
   output$mymap <- renderLeaflet({
     leaflet(df) %>%
       addTiles() %>%
+      setView(lng = 2.2137, lat = 46.2276, zoom = 6) %>% 
       addCircleMarkers(
         lng = ~long,
         lat = ~lat,
@@ -43,7 +45,8 @@ function(input, output, session) {
         color = "blue",
         fillOpacity = 0.8,
         popup = ~paste("<strong>Académie :</strong>", academie, "<br>",
-                       "<strong>Taux de réponse moyen :</strong>", round(taux_reponse_totale), "%")
+                       "<strong>Taux de réponse moyen :</strong>", round(taux_reponse_totale), "%", "<br>",
+                       "<strong>Taux d'insertion moyen :</strong>", round(taux_insertion_moyen), "%")
       )
   })
 }
