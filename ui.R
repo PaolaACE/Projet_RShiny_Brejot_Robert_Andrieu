9c987b9d32ca5c298c41fbce3299c5dd5be2a522
Paola Andrieu <paola.andrieu@agrocampus-ouest.fr>
2024-10-01 17:00:46 +0200

    Appli pour analyse

--- ui.R
+++ ui.R
@@ -19,6 +19,10 @@ fluidPage(
       dataTableOutput(outputId = "dataTable")
     ),
     
+    mainPanel(
+      title = "Test"
+    ),
+    
     tabPanel(
       title = "Emplois par domaines"
     ),
@@ -30,6 +34,53 @@ fluidPage(
       ),
       tabPanel(
         title = "Par emploi"
+      ), 
+      tabPanel(
+        sidebarLayout(
+          sidebarPanel(
+            radioButtons(
+              inputId = "Y",
+              label = "Quelle variable étudier?",
+              choiceNames = list("Salaire brut annuel estimé", 
+                                 "Taux d'emploi", 
+                                 "Taux d'insertion"),
+              choiceValues = list("salaire_brut_annuel_estime",
+                                  "taux_d_emploi", 
+                                  "taux_dinsertion"), 
+              selected = 1
+            ),
+            radioButtons(
+              inputId = "geo",
+              label = "Niveau geographique",
+              choiceNames = list("Académie", "Etablissement"),
+              choiceValues = list("academie", "etablissement"), 
+              selected = 1
+            ),
+          
+            radioButtons(
+              inputId = "sujet",
+              label = "Niveau de précision du sujet",
+              choiceNames = list("Domaine (5)", "Discipline(20)"),
+              choiceValues = list("domaine", "discipline"), 
+              selected = 1
+            ), 
+          
+            sliderInput(
+              inputId = "an",
+              label = "Sur quelle période? ",
+              value = c(2010, 2020),
+              min = 2010, 
+              max = 2020, 
+              round = TRUE, 
+              step = 1, 
+            ), 
+            width = 15
+          ), 
+          mainPanel(
+            verbatimTextOutput(output$res.aov)
+          )
+        )
+        
       )
     )
   )
